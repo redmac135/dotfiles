0f5ead28421c4360d37345c66c13b065fae163e8
Ethan Zhao <ethanzhao1981@gmail.com>
2024-11-02 18:10:00 -0400

    make C-c an alias for Esc

--- lazy/lua/config/settings/mappings.lua
+++ lazy/lua/config/settings/mappings.lua
@@ -20,6 +20,7 @@ vim.keymap.set("i", "<C-h>", "<Left>")
 vim.keymap.set("i", "<C-j>", "<Down>")
 vim.keymap.set("i", "<C-k>", "<Up>")
 vim.keymap.set("i", "<C-l>", "<Right>")
+vim.keymap.set("i", "<C-c>", "<Esc>", { noremap = false, silent = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
