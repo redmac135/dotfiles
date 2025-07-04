b3329b6795a35331b46faca17fc64463af52d9b1
Ethan Zhao <ethanzhao1981@gmail.com>
2025-07-04 18:26:23 -0400

    correct wsl redirect bug sumatra

--- lazy/lua/config/plugins/vimtex.lua
+++ lazy/lua/config/plugins/vimtex.lua
@@ -5,13 +5,21 @@ return {
   lazy = false,
   init = function()
     -- open using sumatraPDF
-    vim.g.vimtex_view_general_viewer = "sumatraPDF"
-    vim.g.vimtex_view_general_options = "-reuse-instance @pdf"
+    vim.g.vimtex_view_general_viewer = "sumatra"
+    vim.g.vimtex_view_general_options = "-reuse-instance"
 
     -- change output dir
     vim.g.vimtex_compiler_latexmk = {
+      backend = "nvim",
       aux_dir = "./build",
       out_dir = "./output",
+      continuous = 1,
+      options = {
+        "-xelatex",
+        "-interaction=nonstopmode",
+        "-synctex=1",
+        "-file-line-error",
+      },
     }
   end,
 }
